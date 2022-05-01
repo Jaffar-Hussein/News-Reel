0793ee7f61134bda8008b426391de8c5a35293f6
Jaffar Hussein <jaffhussein@icloud.com>
2022-05-01 16:52:20 +0200

    ft:Added the classes for the article and the news source

---
+++ app/models/news_source.py
@@ -0,0 +1,11 @@
+class News_Source:
+    """
+    Details for the News Source
+    """
+    def __init__(self,name,url,description,country,language,category):
+        self.name = name
+        self.url = url
+        self.description = description
+        self.country = country
+        self.category = category
+        self.language = language
